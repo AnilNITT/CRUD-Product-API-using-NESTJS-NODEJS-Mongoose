17365eaa9b21a5864ee92c8e024c09242fae510a
Anil Patidar <patidaranil0791@gmail.com>
2023-04-18 15:06:42 +0530

    Update jwtAuth.middleware.ts

--- src/middleware/jwtAuth.middleware.ts
+++ src/middleware/jwtAuth.middleware.ts
@@ -4,5 +4,9 @@ import { Request, Response, NextFunction } from "express";
 export class jwtAuth implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
         console.log(req.headers);
+      /*if(req.headers['api-token'] != 'my-token'){
+            throw new BadRequestException('The token does not match');
+        } */
+        next();
     }
 }
